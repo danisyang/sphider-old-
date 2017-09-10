8f8e16b537bb744b9e2019f987f5a771dc8d594c
Dennis Yang <yzn311976@gmail.com>
2017-09-10 17:44:35 +0800

    9-10 更新 完成显示jd数据页面

--- App/Home/Controller/ShowController.class.php
+++ App/Home/Controller/ShowController.class.php
@@ -1,5 +1,5 @@
 <?php
-
+//show 京东淘宝页面的控制器。
 namespace Home\Controller;
 
 use Vendor\Page;
@@ -9,12 +9,22 @@ class ShowController extends ComController
     public function show_taobao()
     {
 
+
         $this->display();
     }
     public function show_jing()
     {
-      $this->display();
+        $list = M("data")->select();
+       // dump($list);
+       // die();
+        $this->assign("list",$list); //对模板进行赋值，Volist 展示数据
+        $this->display();
     }
+    public function listData(){
 
+
+//        dump($list);die;
+
+    }
 }
 ?>
